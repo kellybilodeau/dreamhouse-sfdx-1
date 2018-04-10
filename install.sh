aa592fe7aa7e4dc92f8aa6de9b1f35a987f6730f
Dave Carroll <djcarroll@gmail.com>
2018-04-10 12:55:34 -0700

    updates

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-sfdx force:org:create -s -a usertest -f config/project-scratch-def.json
+echo "    ===> Creating new scratch org"
+sfdx force:org:create -s -a XOrg -f config/project-scratch-def.json
+echo "    ===> Pushing source..."
 sfdx force:source:push
+echo "    ===> Assigning pemissions..."
 sfdx force:user:permset:assign -n dreamhouse
+echo "    ===> Importing development data..."
 sfdx force:data:tree:import -p assets/data/Broker__c-Property__c-plan.json
+echo "    ===> Environment setup complete."
